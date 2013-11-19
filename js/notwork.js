cb9c841b6daf86905eac2379b659c43f7d07fa2f
kklai <kkrebeccalai@gmail.com>
2013-11-18 23:43:47 -0600

    fix stupid space

--- js/notwork.js
+++ js/notwork.js
@@ -15,7 +15,7 @@ function fillPage() {
 	}
 	for (var i = 3; i < content.length; i++) {
 		row = Math.floor(i/3) + 1;
-		$('#fun .row:nth-child(' + row + ')').append('<div class="span4"><a href="' + content[i].link + '" target="_blank"><h2>' + content[i].title + '</h2></a><a href="' + content[i].link + '" target="_blank"><img src="' + content[i].imglink + '"></a><p>' + conte	nt[i].description + '</p></div>');
+		$('#fun .row:nth-child(' + row + ')').append('<div class="span4"><a href="' + content[i].link + '" target="_blank"><h2>' + content[i].title + '</h2></a><a href="' + content[i].link + '" target="_blank"><img src="' + content[i].imglink + '"></a><p>' + content[i].description + '</p></div>');
 	}
 	footer();
 }
