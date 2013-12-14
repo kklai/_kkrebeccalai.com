70f6de4f315be1e540cdfbdbdf44a4a388b21c25
kklai <kkrebeccalai@gmail.com>
2013-12-13 18:26:35 -0600

    same for notwork

--- js/notwork.js
+++ js/notwork.js
@@ -1,21 +1,10 @@
-var content = [];
-function init() {
-  Tabletop.init({ 
-  	key: '0ArcRX35HpjojdGlZRlBibkRsZ2lFTGVzNXJmOGFwdlE',
-  	callback: function(data, tabletop) { 
-  		content = data;
-  		fillPage();
-  	},
-  	simpleSheet: true } )
-}
-
 function fillPage() {
 	for (var i = 0; i < 3; i++) {
-		$('#fun .row:first').append('<div class="span4"><a href="' + content[i].link + '" target="_blank"><h2>' + content[i].title + '</h2></a><a href="' + content[i].link + '" target="_blank"><img src="' + content[i].imglink + '"></a><p>' + content[i].description + '</p></div>');
+		$('#fun .row:first').append('<div class="span4"><a href="' + notwork[i].link + '" target="_blank"><h2>' + notwork[i].title + '</h2></a><a href="' + notwork[i].link + '" target="_blank"><img src="' + notwork[i].imglink + '"></a><p>' + notwork[i].description + '</p></div>');
 	}
-	for (var i = 3; i < content.length; i++) {
+	for (var i = 3; i < notwork.length; i++) {
 		row = Math.floor(i/3) + 1;
-		$('#fun .row:nth-child(' + row + ')').append('<div class="span4"><a href="' + content[i].link + '" target="_blank"><h2>' + content[i].title + '</h2></a><a href="' + content[i].link + '" target="_blank"><img src="' + content[i].imglink + '"></a><p>' + content[i].description + '</p></div>');
+		$('#fun .row:nth-child(' + row + ')').append('<div class="span4"><a href="' + notwork[i].link + '" target="_blank"><h2>' + notwork[i].title + '</h2></a><a href="' + notwork[i].link + '" target="_blank"><img src="' + notwork[i].imglink + '"></a><p>' + notwork[i].description + '</p></div>');
 	}
 	footer();
 }
@@ -25,5 +14,5 @@ function footer() {
 }
 
 $(document).ready(function(){
-	init();
+	fillPage();
 })
