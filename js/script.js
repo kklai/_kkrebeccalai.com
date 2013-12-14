9e6aa79ec924ee3986f9804b79a720fdb4eec236
kklai <kkrebeccalai@gmail.com>
2013-12-13 18:23:05 -0600

    stop using tabletop cause it's unstable

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-var content = [];
-function init() {
-  Tabletop.init({ 
-  	key: '0ArcRX35HpjojdHIwMmppa0QtY2cyaE1qay1INWJucHc',
-  	callback: function(data, tabletop) { 
-  		content = data;
-  		fillPage();
-  	},
-  	simpleSheet: true } )
-}
-
-// var rowNumber;
-// var row;
-// function drawRows() {
-// 	if (content.length%3 === 0) {
-// 		rowNumber = content.length / 3;
-// 	} else if (content.length%3 != 0) {
-// 		rowNumber = Math.floor(content.length / 3) + 1;
-// 	};
-// 	for (var i = 0; i < rowNumber.length; i++) {
-// 		$('#work').append('<div class="row"></div>');
-// 	}
-// }
-
 function fillPage() {
 	for (var i = 0; i < 3; i++) {
 		$('#work .row:first').append('<div class="span4"><a href="' + content[i].link + '" target="_blank"><h2>' + content[i].title + '</h2></a><a href="' + content[i].link + '" target="_blank"><img src="' + content[i].imglink + '"></a><p><span class="pub">' + content[i].pub + '</span>' + content[i].description + '</p></div>');
@@ -38,5 +14,6 @@ function footer() {
 }
 
 $(document).ready(function(){
-	init();
+	// init();
+	fillPage();
 })
