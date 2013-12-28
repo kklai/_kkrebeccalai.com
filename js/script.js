fea2ea56f70d5bcf9d6d54eb0acd3d2fd4a984cb
kklai <kkrebeccalai@gmail.com>
2013-12-29 02:30:32 +0800

    add rows dynamically

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+var row_number;
+function addRows() {
+	row_number = Math.floor(content.length / 3) + 1;
+	for (var i = 0; i < row_number; i ++) {
+		$('#work').append("<div class='row'></div>");
+	}
+}
+
 function fillPage(input) {
 	for (var i = 0; i < 3; i++) {
 		$('#work .row:first').append('<div class="span4"><a href="' + input[i].link + '" target="_blank"><h2>' + input[i].title + '</h2></a><a href="' + input[i].link + '" target="_blank"><img src="' + input[i].imglink + '"></a><p><span class="pub">' + input[i].pub + '</span>' + input[i].description + '</p></div>');
